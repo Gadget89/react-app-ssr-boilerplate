8af8b96916ace772b660d5bbc1e23faf46dc66f9
Nicolas Roybal <gadget@Nicolass-MacBook-Pro.local>
2018-03-13 11:35:43 -0600

    Initial commit with all needed packages

---
+++ ssr-boilerplate/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-snapshot';
+import App from './App';
+import './index.css';
+// import registerServiceWorker from './registerServiceWorker';
+render(
+    <App />,
+    document.getElementById('root')
+);
+// registerServiceWorker();
